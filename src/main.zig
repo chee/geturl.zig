b65e1ed67b8410a871a00a96e9c160720c26d27b
chee <chee@snoot.club>
2022-07-11 17:59:29 -0500

    comptime

--- src/main.zig
+++ src/main.zig
@@ -6,16 +6,15 @@ const vowels = [_]u8{'a', 'e', 'i', 'o', 'u'};
 fn word() [5]u8 {
     var buffer: [5]u8 = undefined;
 
-    var i: u8 = 5;
+    comptime var i: u8 = 5;
 
-    while (i > 0) {
+    inline while (i > 0): (i -= 1) {
         const int = std.crypto.random.int(u8);
         buffer[i - 1] =
             if (i % 2 == 0)
                 consonants[int % consonants.len]
             else
                 vowels[int % vowels.len];
-        i -= 1;
     }
 
     return buffer;
@@ -28,10 +27,9 @@ pub fn main() anyerror!void {
 
     var times = std.fmt.parseInt(u32, args.next() orelse "1", 0) catch 1;
 
-    while (times > 0) {
+    while (times > 0): (times -= 1) {
         try stdout.print("{s}", .{word()});
-        times -= 1;
-        if (times > 0) {
+        if (times > 1) {
             _ = try stdout.write("-");
         }
     }
