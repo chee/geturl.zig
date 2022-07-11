574598e81932dadc8b3738d04874c7fe908f00cf
chee <chee@snoot.club>
2022-07-11 18:38:00 -0500

    uno lopo

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,26 @@
 const std = @import("std");
 
 const consonants = [_]u8{ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
-const vowels = [_]u8{'a', 'e', 'i', 'o', 'u'};
+const vowels = [_]u8{ 'a', 'e', 'i', 'o', 'u' };
 
-fn word() [5]u8 {
-    var buffer: [5]u8 = undefined;
+fn consonant() u8 {
+    return consonants[std.crypto.random.int(u8) % consonants.len];
+}
 
-    comptime var i: u8 = 5;
+fn vowel() u8 {
+    return vowels[std.crypto.random.int(u8) % vowels.len];
+}
 
-    inline while (i > 0): (i -= 1) {
-        const int = std.crypto.random.int(u8);
-        buffer[i - 1] =
-            if (i % 2 == 0)
-                consonants[int % consonants.len]
-            else
-                vowels[int % vowels.len];
-    }
-
-    return buffer;
+fn word() std.fmt.BufPrintError![5]u8 {
+    var w: [5]u8 = undefined;
+    _ = try std.fmt.bufPrint(&w, "{c}{c}{c}{c}{c}", .{
+        consonant(),
+        vowel(),
+        consonant(),
+        vowel(),
+        consonant()
+    });
+    return w;
 }
 
 pub fn main() anyerror!void {
@@ -27,7 +30,7 @@ pub fn main() anyerror!void {
 
     var times = std.fmt.parseInt(u32, args.next() orelse "1", 0) catch 1;
 
-    while (times > 0): (times -= 1) {
+    while (times > 0) : (times -= 1) {
         try stdout.print("{s}", .{word()});
         if (times > 1) {
             _ = try stdout.write("-");
