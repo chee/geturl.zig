a25a2b405c19ed3cd65934ade2cfe838b5b6e717
chee <chee@snoot.club>
2022-07-21 22:01:12 -0500

    remove vendor dir

--- vendor/mimetypes/src/main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub const mimetypes = @import("mimetypes.zig");
-
-comptime {
-    std.testing.refAllDecls(@This());
-}
--- vendor/mimetypes/zig.mod
+++
@@ -1,4 +0,0 @@
-id: 9gsl54kagdcpw98kwv4e65yddwhm2m4a9mlxw5r97gn9x0v4
-name: mimetypes
-main: src/main.zig
-dependencies:
